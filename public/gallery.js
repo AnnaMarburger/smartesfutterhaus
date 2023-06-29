bf69f19aeb29eef341e4cd3aafa5b258ca3a7e61
Anna Marburger <anna.marburger@uni-ulm.de>
2023-06-29 15:11:08 +0200

    fixed typo

--- public/gallery.js
+++ public/gallery.js
@@ -89,7 +89,7 @@ async function loadJSONData(filter, storage) {
     const response = await fetch("./data.json");
     datajson = response.json();
     data = datajson.data;
-    if(data === undefined || data.length == 0){
+    if(data.length < 1){
         //no data there yet
         var container = document.getElementById("gallery-container");
 
